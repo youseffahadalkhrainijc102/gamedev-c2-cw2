4725a7816c927f4bc56ee633fb4d0022f3e27dfd
youseffahadalkhrainijc102 <70811878+youseffahadalkhrainijc102@users.noreply.github.com>
2022-03-08 20:35:52 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace c2_cw2
+{
+
+    class Program
+    {
+        
+            
+        
+        static void Main(string[] args)
+        {
+
+            string Herosname = "yoda";
+            string enemy = "vader";
+            int age = 1200;
+            int enemyage = 1000;
+            string superpower1 = "superjump";
+            string superpower2 = "flight";
+
+
+
+            ageDiffrence(age, enemyage);
+            void ageDiffrence(int age , int enemyage) {
+
+                if (age > enemyage)
+                {
+
+                    Console.WriteLine(Herosname + "is older than" + enemy);
+
+                }
+                else if (age < enemyage)
+                {
+
+                    Console.WriteLine(enemy + "is younger than" + Herosname);
+
+
+                }
+        }
+
+        }
+
+    }
+
+
+
+}
